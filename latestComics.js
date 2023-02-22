31847730bd7178e71ea2472eb2128dcf1d3e973e
TheLost109 <60025456+TheLost109@users.noreply.github.com>
2023-02-22 19:44:55 +0800

    Create latestComics.js

---
+++ latestComics.js
@@ -0,0 +1,9 @@
+var DSNum = 1;
+var comicDS1_1 = ['https://p5.toutiaoimg.com/origin/1385500045d4a7a4303bb',
+                  'https://p5.toutiaoimg.com/origin/1389c00017f8982695fa2',
+                  'https://p5.toutiaoimg.com/origin/137c90002c72d6a7ff497',
+                  'https://p5.toutiaoimg.com/origin/137b2000283f4f480454d',
+                  'https://p5.toutiaoimg.com/origin/fecd0002f20d3ed33119',
+                  'https://p5.toutiaoimg.com/origin/137fd0002d110ce3827e1',
+                  'https://p5.toutiaoimg.com/origin/1385d00020803c62e03c2',
+                  'https://p5.toutiaoimg.com/origin/ff2c00033b912ec0aa92']
